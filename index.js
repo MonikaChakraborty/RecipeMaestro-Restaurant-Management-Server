7c2efbde443cf040a5d06c7892c13885078f5ebe
MonikaChakraborty <mchakraborty7516@gmail.com>
2023-11-07 23:12:20 +0600

    added pagination and search functionality for all food page

--- index.js
+++ index.js
@@ -1,50 +1,135 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+// const express = require("express");
+// const cors = require("cors");
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// const { parse } = require("dotenv");
+// require("dotenv").config();
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// // middleware
+// app.use(cors());
+// app.use(express.json());
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63dg6sa.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+//     const allFoodItemsCollection = client
+//       .db("restaurantManagement")
+//       .collection("allFoodItems");
+
+
+//     // show all food items
+//     app.get("/allFoodItems", async (req, res) => {
+//       const page = parseInt(req.query.page);
+//       const size = parseInt(req.query.size);
+//       console.log('pagination query', page, size);
+//       const cursor = allFoodItemsCollection.find()
+//       .skip(page * size)
+//       .limit(size)
+//       const result = await cursor.toArray();
+//       res.send(result);
+//     });
+
+    
+
+
+
+//     // pagination
+//     app.get("/allFoodItemsCount", async (req, res) => {
+//       const count = await allFoodItemsCollection.estimatedDocumentCount();
+//       res.send({ count });
+//     });
+
+    
+
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+// app.get("/", (req, res) => {
+//   res.send("restaurant management server is running");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Restaurant Management Server is running on port ${port}`);
+// });
+
+
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63dg6sa.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
+    const allFoodItemsCollection = client
+      .db("restaurantManagement")
+      .collection("allFoodItems");
 
-    const allFoodItemsCollection = client.db('restaurantManagement').collection('allFoodItems');
+    // show all food items with search
+    app.get("/allFoodItems", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const searchTerm = req.query.searchTerm;
 
-    app.get('/allFoodItems', async(req, res) => {
-        const cursor = allFoodItemsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+      let query = {};
+      if (searchTerm) {
+        query = { foodName: { $regex: new RegExp(searchTerm, "i") } };
+      }
 
+      const cursor = allFoodItemsCollection.find(query)
+        .skip(page * size)
+        .limit(size);
 
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-
-
-
-
-
-
+    // pagination
+    app.get("/allFoodItemsCount", async (req, res) => {
+      const count = await allFoodItemsCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -54,15 +139,14 @@ async function run() {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('restaurant management server is running')
-})
+app.get("/", (req, res) => {
+  res.send("restaurant management server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Restaurant Management Server is running on port ${port}`);
-})
+  console.log(`Restaurant Management Server is running on port ${port}`);
+});
+
