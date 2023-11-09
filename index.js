e0b186d47c51a5230468d939fc48525bf236e4e8
MonikaChakraborty <mchakraborty7516@gmail.com>
2023-11-09 19:07:36 +0600

    cleared some commented code

--- index.js
+++ index.js
@@ -1,72 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// const { parse } = require("dotenv");
-// require("dotenv").config();
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// // middleware
-// app.use(cors());
-// app.use(express.json());
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63dg6sa.mongodb.net/?retryWrites=true&w=majority`;
-
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-
-//     const allFoodItemsCollection = client
-//       .db("restaurantManagement")
-//       .collection("allFoodItems");
-
-//     // show all food items
-//     app.get("/allFoodItems", async (req, res) => {
-//       const page = parseInt(req.query.page);
-//       const size = parseInt(req.query.size);
-//       console.log('pagination query', page, size);
-//       const cursor = allFoodItemsCollection.find()
-//       .skip(page * size)
-//       .limit(size)
-//       const result = await cursor.toArray();
-//       res.send(result);
-//     });
-
-//     // pagination
-//     app.get("/allFoodItemsCount", async (req, res) => {
-//       const count = await allFoodItemsCollection.estimatedDocumentCount();
-//       res.send({ count });
-//     });
-
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// app.get("/", (req, res) => {
-//   res.send("restaurant management server is running");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Restaurant Management Server is running on port ${port}`);
-// });
-
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -166,14 +97,14 @@ async function run() {
       res.send(result);
     });
 
-    // food purchase/order page
+
+    // food purchase page
     app.get("/foodPurchase/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await allFoodItemsCollection.findOne(query);
       res.send(result);
     });
-
 
 
 
@@ -216,7 +147,6 @@ async function run() {
 
 
 
-
     // users
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -224,6 +154,7 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
 
     // add food items to allFoodItems Collection
     app.post("/addFoodItems", async (req, res) => {
